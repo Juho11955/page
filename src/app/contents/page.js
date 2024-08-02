f8f121075f14a618cbe6eeac1064dc86ba5dd090
Juho <orot1195@gmail.com>
2024-08-03 00:38:01 +0900

    test commit

--- src/app/contents/page.js
+++ src/app/contents/page.js
@@ -1,11 +1,11 @@
 import Image from "next/image";
-import styles from "./page.module.css";
+import styles from "../page.module.css";
 
-export default function Home() {
+export default function Contents() {
   return (
     <main className={styles.main}>
       <div className={styles.description}>
-        
+        jude 
       </div>
     </main>
   );

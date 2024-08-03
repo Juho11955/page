dc13b0dd897f1c913564f7e512c03f22d1899bdb
Juho <orot1195@gmail.com>
2024-08-03 12:23:00 +0900

    test commit

---
+++ src/layout/AppLayout.js
@@ -0,0 +1,10 @@
+
+export default function AppLayout({ children }) {
+    return (
+        <>
+            <div className="app">
+                {children}
+            </div>
+        </>
+    );
+}
